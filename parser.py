0b2d232d06e2b7686ae716fe4fe86f98202db899
Tuan Tran <atran18@ucsc.edu>
2021-04-18 18:14:41 -0700

    getting byte order from sys

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3.8
+import sys
 
 header_file = "blockchain_headers"
+byteorder = sys.byteorder
 with open(header_file, "rb") as f:
     block_num = 1
     print("%7s %32s %32s" % ("num", "block time", "block difficulty"))
@@ -14,14 +16,14 @@ with open(header_file, "rb") as f:
         difficulty_str = f.read(4)
         nonce = f.read(4)
 
-        version_num = int.from_bytes(version, "little")
-        block_time = int.from_bytes(block_time_str, "little")
-        block_difficulty = int.from_bytes(difficulty_str, "little")
+        version_num = int.from_bytes(version, byteorder)
+        block_time = int.from_bytes(block_time_str, byteorder)
+        block_difficulty = int.from_bytes(difficulty_str, byteorder)
 
         print(block_num, ": ", block_time, " ", block_difficulty)
 
         block_num = block_num + 1
 
-        # if block_num == 5:
-        #     break
+        if block_num == 5:
+            break
 
